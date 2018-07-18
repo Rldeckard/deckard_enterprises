6d0b4901003967c089b464626046911daa821397
root <root@webserver.deckard>
2018-07-17 23:58:05 -0400

    Added home page

--- header.php
+++ header.php
@@ -1,3 +1,15 @@
+<link rel="icon" href="logo.png">
+<div style="text-align:left;font-size:40px;padding-left:10px">
+  <div style="display:inline-block;text-align:center">
+    <label style="display:block;color:blue;width:100%;font-family:Brush Script MT, Brush Script Std, cursive">
+      Deckard
+    </label>
+    <label style="display:block;padding:0;font-size:40%;width:100%">
+      E n t e r p r i s e s
+    </label>
+  </div>
+</div>
+
 <?php
 $path = $_SERVER['PHP_SELF'];
 ?>
--- index.php
+++ index.php
@@ -1,26 +1,55 @@
-<?php
-
-include ('/header.php');
-print $path;
-?>
 <html>
   <head>
-    <link rel="icon" href="php_logo.jpg">
+  <style>
+  th {
+    background-color: #19198c;
+    color: white;
+    padding: 10px;
+    margin: 0px;
+    width: 1%;
+    text-align: center;
+    }
+  a {
+    color: black;
+    text-decoration: none;
+    padding: 10px;
+  }
+  a:active {
+    color: black;
+  }
+  td:hover {
+    background-color: lightgray;
+    cursor: pointer;
+    box-shadow: 2px 2px 10px;
+  }
+  td:active {
+    padding: 8px;
+    background-color: gray;
+    box-shadow: 1px 1px 20px;
+    border-radius: 1px 1px 1px 1px;
+  }
+  td {
+    padding: 10px;
+  }
+  </style>
+  <?php include('header.php'); ?>
     <title>
       Deckard Enterprises
     </title>
   </head>
   <body style="margin:0"> <! Margin needs done here to eliminate white space>
-    <div style="text-align:center;background-color:#19198c;;padding:10px">
+    <div style="text-align:center;background-color:#19198c;;padding:15px">
       <span style="color:white">Welcome to Deckard Enterprises!</span>
     </div>
-    <div style="font-size:80px;text-align:center;padding:10px">
-      <label style="display:block;color:blue;width:100%;font-family:Brush Script MT, Brush Script Std, cursive">
-        Deckard
-      </label>
-      <label style="padding:0;font-size:40%;width:100%">
-        E n t e r p r i s e s
-      </label>
+    <?php $projects = scandir('/var/www/html'); ?>
+    <div style="margin:0 auto;width:60%;text-align:center;padding-top:5%">
+      <table style="text-align:center">
+        <tr>
+          <th>Current Projects</th><th>Helpful Links</th><th>Future Projects</th>
+        </tr>
+        <td><a target="_external" href="ticket_tracker">935TH Ticket Tracker</a></td><td><a target="_external" href="https://github.com/Rldeckard/ticket_tracker">Ticket Tracker Files</a></td><td></td>
+        </tr>
+      </table>
     </div>
   </body>
 </html>
