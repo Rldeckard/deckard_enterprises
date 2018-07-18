e2acc36e4581fefa68198495b81030d587d275c2
root <root@webserver.deckard>
2018-07-17 22:36:03 -0400

    home page

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
+<?php
+
+include ('/header.php');
+print $path;
+?>
 <html>
-<head>
-<title>
-Deckard, Ryan L.
-</title>
-<head>
-<body>
-<span>Welcome to my website!</span>
-</body>
+  <head>
+    <link rel="icon" href="php_logo.jpg">
+    <title>
+      Deckard Enterprises
+    </title>
+  </head>
+  <body style="margin:0"> <! Margin needs done here to eliminate white space>
+    <div style="text-align:center;background-color:#19198c;;padding:10px">
+      <span style="color:white">Welcome to Deckard Enterprises!</span>
+    </div>
+    <div style="font-size:80px;text-align:center;padding:10px">
+      <label style="display:block;color:blue;width:100%;font-family:Brush Script MT, Brush Script Std, cursive">
+        Deckard
+      </label>
+      <label style="padding:0;font-size:40%;width:100%">
+        E n t e r p r i s e s
+      </label>
+    </div>
+  </body>
 </html>
 
 
